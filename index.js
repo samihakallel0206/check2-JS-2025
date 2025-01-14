ee03f1bd6829d0c07a0e988d196b60de08ecface
samihakallel0206 <samkallel@hotmail.fr>
2025-01-14 21:58:46 +0100

    check

--- index.js
+++ index.js
@@ -118,7 +118,7 @@ function factoriel(n) {
 }
 
 var resultat = factoriel(4);
-console.log(resultat);
+// console.log(resultat);
 
 function factorielFOR(n) {
   var fact = 1;
@@ -127,5 +127,106 @@ function factorielFOR(n) {
   }
   return fact;
 }
-console.log(factorielFOR(5));
+// console.log(factorielFOR(5));
 // ****************************************************
+//nombre premier
+// FOR
+function premier(n) {
+  for (var i = 2; i <= n / 2; i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+// console.log(premier(7))
+
+// D21 = { 1, 3, 7, 21}
+// D25 = { 1, 5, 25}
+// D37 = {  2,3,4,5,6,7,8,9....36
+//   1, 37,
+// }
+
+function premier(n) {
+  // for (var i = 2; i <= n/2; i++){
+  //   if (n % i === 0) { return false }
+
+  // }
+  var i = 2;
+  while (i <= n / 2) {
+    if (n % i === 0) {
+      return false;
+    }
+
+    //changement
+    i = i + 1;
+  }
+
+  return true;
+}
+
+// console.log(premier(7));
+
+//Fibonacci
+// u0 = 0;
+// u1 = 1;
+// un2 = un0 + un1 = 0 + 1
+// un3 = un1 + un2 = 1 + 1 =2
+// un4 = un2 + in3 = 1 + 2 =3
+// un5 = 3 + 2= 5
+// 0 1 1 2 3 5 8 13 21 34 55 89
+function Fibonacci(n) {
+  var tab = [0, 1];
+  if (n === 0) return tab[0];
+  if (n === 1) return tab;
+  for (var i = 2; i <= n; i++) {
+    tab[i] = tab[i - 1] + tab[i - 2];
+  }
+  return tab;
+}
+
+// console.log(Fibonacci(5));
+// Tri par insertion.
+function TriInsertion(tab) {
+  // var key, i, j;
+  for (var i = 1; i < tab.length; i++) {
+    key = tab[i];
+    j = i - 1;
+    while (j >= 0 && tab[j] > key) {
+      tab[j + 1] = tab[j];
+      j--;
+    }
+    tab[j + 1] = key;
+  }
+  return tab;
+}
+
+// console.log([6,5,3,1,8])
+
+// La longueur de la phrase (le nombre de caractères).
+// Le nombre de mots dans la phrase (en supposant que les mots sont séparés par un seul espace).
+// Le nombre de voyelles dans la phrase.
+
+function algo1JS(ph) {
+  var cptCar = ph.length;
+  var cptMot = ph.split(" ").length;
+  var cptV = 0;
+  // var voyelle = ['a', 'e', 'i', 'u', 'o'];
+  for (var i = 0; i < ph.length; i++) {
+    if (
+      ph[i].toLowerCase() === "a" ||
+      ph[i].toLowerCase() === "u" ||
+      ph[i].toLowerCase() === "i" ||
+      ph[i].toLowerCase() === "e" ||
+      ph[i].toLowerCase() === "o"
+    )
+      cptV++;
+  }
+
+  return `Le nombre de caracère dans cette phrase est: ${cptCar},
+  le nombre de mot est : ${cptMot} et le nombre de voyelle est ${cptV} `;
+}
+
+// console.log(algo1JS("On est fatigué, fin de journée"));
